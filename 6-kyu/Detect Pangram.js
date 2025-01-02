b311b4e673b47de3bddb33dd1eb2d5bcc57a9edf
undefined64 <kirillpretty@gmail.com>
2025-01-03 01:13:23 +0300

    refactor: made solution look simpler

--- 6-kyu/Detect Pangram.js
+++ 6-kyu/Detect Pangram.js
@@ -6,41 +6,8 @@
 // Return True if it is, False if not. Ignore numbers and punctuation.
 
 function isPangram(string) {
-  const objCounter = {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0,
-    h: 0,
-    i: 0,
-    j: 0,
-    k: 0,
-    l: 0,
-    m: 0,
-    n: 0,
-    o: 0,
-    p: 0,
-    q: 0,
-    r: 0,
-    s: 0,
-    t: 0,
-    u: 0,
-    v: 0,
-    w: 0,
-    x: 0,
-    y: 0,
-    z: 0,
-  };
-
   string = string.toLowerCase();
-  for (let i = 0; i < string.length; i += 1) {
-    if (string[i] in objCounter) {
-      objCounter[string[i]] += 1;
-    }
-  }
-
-  return Object.values(objCounter).filter((value) => value >= 1).length === 26;
+  return 'abcdefghijklmnopqrstuvwxyz'
+    .split('')
+    .every((letter) => string.indexOf(letter) !== -1);
 }
