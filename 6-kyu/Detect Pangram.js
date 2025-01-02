916098eee8f672b9ddee14d41b229a9a1965d6ae
undefined64 <kirillpretty@gmail.com>
2025-01-02 23:56:35 +0300

    feat: solve detect pangram

---
+++ 6-kyu/Detect Pangram.js
@@ -0,0 +1,46 @@
+// A pangram is a sentence that contains every single letter of the alphabet at least once.
+// For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram,
+// because it uses the letters A-Z at least once (case is irrelevant).
+
+// Given a string, detect whether or not it is a pangram.
+// Return True if it is, False if not. Ignore numbers and punctuation.
+
+function isPangram(string) {
+  const objCounter = {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0,
+    g: 0,
+    h: 0,
+    i: 0,
+    j: 0,
+    k: 0,
+    l: 0,
+    m: 0,
+    n: 0,
+    o: 0,
+    p: 0,
+    q: 0,
+    r: 0,
+    s: 0,
+    t: 0,
+    u: 0,
+    v: 0,
+    w: 0,
+    x: 0,
+    y: 0,
+    z: 0,
+  };
+
+  string = string.toLowerCase();
+  for (let i = 0; i < string.length; i += 1) {
+    if (string[i] in objCounter) {
+      objCounter[string[i]] += 1;
+    }
+  }
+
+  return Object.values(objCounter).filter((value) => value >= 1).length === 26;
+}
